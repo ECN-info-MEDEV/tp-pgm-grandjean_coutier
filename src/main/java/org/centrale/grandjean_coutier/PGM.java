815a72d0de692a6f7a03d685dfa4ce2fa9ee31ea
ManonC12 <manon-c.coutier@wanadoo.fr>
2022-12-01 10:24:53 +0100

    Constructeur+Getters+Setters PGM

--- src/main/java/org/centrale/grandjean_coutier/PGM.java
+++ src/main/java/org/centrale/grandjean_coutier/PGM.java
@@ -24,6 +24,66 @@ public class PGM {
     /**
      * Tableau des valeurs de chaque pixel
      */
-    private ArrayList<ArrayList<Integer>> image;
+    private ArrayList<Integer> image;
+    
+    //CONSTRUCTEUR
+    /**
+     * Constructeur par défaut
+     */
+    public PGM(){
+        this.taille_x = 0;
+        this.taille_y = 0;
+        this.image = new ArrayList<>();
+    }
+    
+    //GETTERS ET SETTERS
+    /**
+     * Getter taille_x
+     * @return taille_x
+     */
+    public int getTaille_x() {
+        return taille_x;
+    }
+    
+    /**
+     * Setter taille_x
+     * @param taille_x nouvelle valeur de taille_x
+     */
+    public void setTaille_x(int taille_x) {
+        this.taille_x = taille_x;
+    }
+    
+    /**
+     * Getter taille_y
+     * @return taille_y
+     */
+    public int getTaille_y() {
+        return taille_y;
+    }
+    
+    /**
+     * Setter taille_y
+     * @param taille_y nouvelle valeur de taille_y
+     */
+    public void setTaille_y(int taille_y) {
+        this.taille_y = taille_y;
+    }
+    
+    /**
+     * Getter image
+     * @return image
+     */
+    public ArrayList<Integer> getImage() {
+        return image;
+    }
+    
+    /**
+     * Setter image
+     * @param image nouvelle valeur de image
+     */
+    public void setImage(ArrayList<Integer> image) {
+        this.image = image;
+    }
+    
     
 }
